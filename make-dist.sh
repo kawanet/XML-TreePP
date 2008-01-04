38b4888d4fa9e673a1046f5e7f7c3eed8cabaeb2
kawanet <u-suke@kawa.net>
2008-01-04 15:57:58 +0000

    Copyright 2006-2008
    git-svn-id: https://xml-treepp.googlecode.com/svn/trunk/XML-TreePP@9 4b0939d0-ee41-0410-9078-99bb3c2717fb

--- make-dist.sh
+++ make-dist.sh
@@ -10,7 +10,9 @@ doit () {
 }
 
 rdf=t/example/index.rdf
-doit wget --timestamping -O $rdf http://www.kawa.net/rss/index-e.rdf
+doit wget -O $rdf~ http://www.kawa.net/rss/index-e.rdf
+diff $rdf $rdf~ > /dev/null || doit /bin/mv -f $rdf~ $rdf
+/bin/rm -f $rdf~
 
 egrep -v '^t/.*\.t$' MANIFEST > MANIFEST~
 ls t/*.t >> MANIFEST~
@@ -24,9 +26,13 @@ doit make disttest
 
 main=`grep 'lib/.*pm$' < MANIFEST | head -1`
 [ "$main" == "" ] && die "main module is not found in MANIFEST"
-doit pod2text $main > README
+doit pod2text $main > README~
+diff README README~ > /dev/null || doit /bin/mv -f README~ README
+/bin/rm -f README~
 
 doit make dist
-doit /bin/rm -fr blib pm_to_blib
+[ -d blib ] && doit /bin/rm -fr blib
+[ -f pm_to_blib ] && doit /bin/rm -f pm_to_blib
+[ -f Makefile.old ] && doit /bin/rm -f Makefile.old
 
 ls -lt *.tar.gz | head -1
